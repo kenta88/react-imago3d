2f8349308e868c294808480e0394b031adf79fc5
kenta88 <solinvictus.adc@gmail.com>
2017-06-18 18:00:33 +0200

    added style component and created canvas component

--- src/modules/imago/connectors/Imago3d.jsx
+++ src/modules/imago/connectors/Imago3d.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
+import autobind from 'autobind-decorator';
+import {
+    Row,
+    Col,
+} from 'react-materialize';
 
 import Layout from '../../../connectors/Layout';
+import Canvas from '../components/Canvas';
 
 class Imago3d extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            canvasContainerSize: null,
+        };
+    }
+
+    @autobind
+    onRefCanvasContainer(container) {
+        const canvasContainer = container.querySelector('#canvasContainer');
+        this.setState({
+            canvasContainerSize: canvasContainer.getBoundingClientRect(),
+        });
     }
 
     render() {
         return (
             <Layout>
-                <h2>this is a layout</h2>
+                <div ref={this.onRefCanvasContainer}>
+                    <Row>
+                        <Col
+                            s={12}
+                            id="canvasContainer"
+                        >
+                            {this.state.canvasContainerSize ? (
+                                <Canvas
+                                    width={this.state.canvasContainerSize.width - 24}
+                                    height={window.innerHeight - 84}
+                                />
+                            ) : null}
+                        </Col>
+                    </Row>
+                </div>
             </Layout>
         );
     }

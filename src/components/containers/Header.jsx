ea3e8926333c4cd350d7acc3f471e11fb37c30ab
Alessio di Crescenzo <a.dicrescenzo@elsevier.com>
2019-03-27 16:56:35 +0100

    eslint

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -2,6 +2,15 @@
 import autobind from 'autobind-decorator';
 import { connect } from 'react-redux';
 import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
+
 
 import { OBJECTS3D } from '../../constants';
 import SAMPLE from '../../constants/sample';
@@ -45,72 +54,89 @@ class Header extends React.Component {
     };
   }
 
-    @autobind
+  @autobind
   onClick3dObject(type) {
     this.props.createObject(OBJECTS3D[type]);
     this.closeDrawner();
   }
 
-    @autobind
-    openDrawner() {
+  @autobind
+  openDrawner() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  @autobind
+  saveToLocalStorage() {
+    const objects = this.props.objects.toJS();
+    localStorage.setItem('objects', JSON.stringify(objects));
+    this.setState({
+      modal: true,
+      isSaving: true,
+    });
+  }
+
+  @autobind
+  // eslint-disable-next-line
+  saveSampleToLocalStorage() {
+    localStorage.setItem('objects', JSON.stringify(SAMPLE));
+    this.setState({
+      modal: true,
+    });
+  }
+
+  @autobind
+  resetLocalStorage() {
+    localStorage.setItem('objects', JSON.stringify([]));
+    this.setState({
+      modal: true,
+    });
+  }
+
+  @autobind
+  closeSaveModal(e) {
+    if (!this.state.isSaving) {
+      location.reload();
+    } else {
       this.setState({
-        open: true,
+        modal: false,
+        isSaving: false,
       });
     }
+  }
 
-    @autobind
-    saveToLocalStorage() {
-      const objects = this.props.objects.toJS();
-      localStorage.setItem('objects', JSON.stringify(objects));
-      this.setState({
-        modal: true,
-        isSaving: true,
-      });
-    }
+  @autobind
+  closeDrawner() {
+    this.setState({
+      open: false,
+    });
+  }
 
-    @autobind
-    // eslint-disable-next-line
-    saveSampleToLocalStorage() {
-      localStorage.setItem('objects', JSON.stringify(SAMPLE));
-      this.setState({
-        modal: true,
-      });
-    }
-
-    @autobind
-    resetLocalStorage() {
-      localStorage.setItem('objects', JSON.stringify([]));
-      this.setState({
-        modal: true,
-      });
-    }
-
-    @autobind
-    closeSaveModal() {
-      if (!this.state.isSaving) {
-        location.reload();
-      } else {
-        this.setState({
-          modal: false,
-          isSaving: false,
-        });
-      }
-    }
-
-    @autobind
-    closeDrawner() {
-      this.setState({
-        open: false,
-      });
-    }
-
-    render() {
-      return (
-        <>
-          <h2>test</h2>
-        </>
-      );
-    }
+  render() {
+    const { modal } = this.state;
+    return (
+      <div>
+        <Dialog
+          open={modal}
+          onClose={this.closeSaveModal}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Local storage updated!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.closeSaveModal} autoFocus>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -3,79 +3,79 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpackConfig = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-hot-middleware/client',
-        'babel-polyfill',
-        './src/index.jsx',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './src/index.jsx',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.join(__dirname, 'src'),
     ],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-        publicPath: '/static/',
-    },
-    resolve: {
-        modules: [
-            'node_modules',
-            path.join(__dirname, 'src'),
-        ],
-        extensions: [
-            '*',
-            '.js',
-            '.json',
-            '.jsx',
-        ],
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js(x?)$/,
-                loader: 'eslint-loader',
-                exclude: /(node_modules)/,
-            },
-            { test: /\.json$/, loader: 'json-loader' },
-            { test: /\.(jpg|png|gif|svg)$/, loader: 'file-loader' },
-            {
-                test: /\.js(x?)$/,
-                loader: 'babel-loader',
-                include: /(src)/,
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.(s?)css$/,
-                loaders: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            IS_PRODUCTION: JSON.stringify(false),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.ProvidePlugin({
-            React: 'react',
-            ReactDOM: 'react-dom',
-        }),
-        new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
-            root: path.resolve(__dirname, 'public'),
-            verbose: false,
-            dry: false, // true for simulation
-        }),
+    extensions: [
+      '*',
+      '.js',
+      '.json',
+      '.jsx',
     ],
-    devtool: 'eval',
+  },
+  module: {
+    rules: [
+      // {
+      //   enforce: 'pre',
+      //   test: /\.js(x?)$/,
+      //   loader: 'eslint-loader',
+      //   exclude: /(node_modules)/,
+      // },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(jpg|png|gif|svg)$/, loader: 'file-loader' },
+      {
+        test: /\.js(x?)$/,
+        loader: 'babel-loader',
+        include: /(src)/,
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(s?)css$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_PRODUCTION: JSON.stringify(false),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      React: 'react',
+      ReactDOM: 'react-dom',
+    }),
+    new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
+      root: path.resolve(__dirname, 'public'),
+      verbose: false,
+      dry: false, // true for simulation
+    }),
+  ],
+  devtool: 'eval',
 };
 
 module.exports = webpackConfig;
